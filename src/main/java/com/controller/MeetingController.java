3c3a4480c7e25a349864225aa35457b364e3b6cc
JACOBS-PC\Jacob <jponsa15@student.aau.dl>
2018-05-16 19:36:48 +0200

    changed in meetingView and controller

--- src/main/java/com/controller/MeetingController.java
+++ src/main/java/com/controller/MeetingController.java
@@ -1,11 +1,11 @@
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import sun.tools.jar.Main;
 
 import java.awt.*;
@@ -19,51 +19,47 @@ import java.util.ResourceBundle;
 
 public class MeetingController implements Initializable {
     ObservableList<String> meetingList = FXCollections.observableArrayList();
-    ObservableList<String> arranged_meetingList = FXCollections.observableArrayList();
+    final ObservableList<String> arranged_meetingList = FXCollections.observableArrayList();
 
     int potentialMeetings = 10;
 
     @FXML
     public ChoiceBox<String> elevList;
-    public ChoiceBox meetingViewer;
     public DatePicker Datepicker;
+    public ListView listView;
 
-    private void handlePotentialMeeting(){
-       DatabaseController.loadMeetingModel();
+    private void handlePotentialMeeting() {
+        DatabaseController.loadMeetingModel();
         ArrayList<String> studentList = MeetingModel.participatingStudent_id;
         ArrayList<Date> meeting_time = MeetingModel.getMeetingTime();
         String[] user_id = MeetingModel.getParticipatingCoordinator().toArray(new String[0]);
-        for (int i=0; i<studentList.size(); i++) {
+        for (int i = 0; i < studentList.size(); i++) {
             int readyForMeeting = Integer.parseInt(user_id[i]);
             if (readyForMeeting != 0) {
-                System.out.println(studentList.get(i));
-                arranged_meetingList.removeAll(arranged_meetingList);
-                arranged_meetingList.addAll(studentList.get(i));
-                arranged_meetingList.addAll(String.valueOf(meeting_time.get(i)));
-                meetingViewer.getItems().addAll(arranged_meetingList);
+                arranged_meetingList.add("Elev: "+studentList.get(i)+" møde dato: "+ meeting_time.get(i));
+                //arranged_meetingList.addAll(String.valueOf(meeting_time.get(i)));
+                ///meetingViewer.getItems().addAll(arranged_meetingList);
 
             } else {
             }
 
 
         }
-        }
-
-
+    }
 
 
     private void showMeetings() throws SQLException {
         DatabaseController.loadMeetingModel();
-        String[] studentList = MeetingModel.participatingStudent_id.toArray(new String[0]);
+        ArrayList<String> studentList = MeetingModel.participatingStudent_id;
         String[] user_id = MeetingModel.getParticipatingCoordinator().toArray(new String[0]);
-        for (int i = 0; i < studentList.length; i++) {
+        for (int i = 0; i < studentList.size(); i++) {
             int readyForMeeting = Integer.parseInt(user_id[i]);
-            //System.out.println(user_id[i]);
             if ( readyForMeeting == 0 ){
                 meetingList.removeAll(meetingList);
-                meetingList.addAll(studentList);
+                meetingList.addAll(studentList.get(i));
                 elevList.getItems().addAll(meetingList);
             }else {
+
             }
         }
     }
@@ -71,6 +67,7 @@ public class MeetingController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            listView.setItems(arranged_meetingList);
             showMeetings();
             handlePotentialMeeting();
         } catch (SQLException e) {
