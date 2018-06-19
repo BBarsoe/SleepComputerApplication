9340681717ffc9296840554b530c5410b2c7d55d
BBarsoe <alauri15@student.aau.dk>
2018-06-19 17:08:35 +0200

    Tilføjet JAVADOC

--- src/main/java/com/model/StudentListModel.java
+++ src/main/java/com/model/StudentListModel.java
@@ -4,14 +4,23 @@ import java.util.List;
 class StudentListModel {
     static ArrayList<String> studentList_id;
 
+    /**
+     * Sætter attributten studentList_id af typen ArrayList til input argumentet studentList_id
+     */
     static void setStudentList_id(ArrayList<String> studentList_id){
         StudentListModel.studentList_id = studentList_id;
     }
 
+    /**
+     * Returnere attributten studentList_id af typen ArrayList.
+     */
     static ArrayList<String> getStudentList_id(){
         return studentList_id;
     }
 
+    /**
+     * Kalder metoden loadStudentListModel() i klassen DatabaseController, som har til formål at udfylde denne model med data fra databasen
+     */
     void loadModel(){
         DatabaseController.loadStudentListModel();
     }
