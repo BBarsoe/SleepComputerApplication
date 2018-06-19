aab3221e392b0ad2c353fc372979e7547548e516
JACOBS-PC\Jacob <jponsa15@student.aau.dl>
2018-06-19 09:45:30 +0200

    added comments to all method

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -23,6 +23,12 @@ public class MainController extends Application {
 		launch(args);
 	}
 
+	/**
+	 * Gennem metoden start sættes vores primaryStage, med titlen 'Sleep Computer Application'.
+	 * Her efter gøres der således vores primaryStage ikke kan justeres, men er den samme størrelse.
+	 * Med metoden goToLogin(), som kaldes fra Logincontrolleren, sættes loginView til at være primaryStage.
+	 * Med metoden initDatabaseConnection() skabes der forbindelse til databasen.
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -32,11 +38,23 @@ public class MainController extends Application {
 		initDatabaseConnection();
 	}
 
+	/**
+	 * variablen con er af typen java sql connection, hvor denne variable sættes til at være lig med
+	 * metoden connect(), der kaldes fra klassen DatabaseController.
+	 * Dette gøres for at kunne oprette forbindelse til databasen.
+	 */
 	private void initDatabaseConnection() {
 		con = DatabaseController.connect();
 	}
 
-
+	/**
+	 * Med metoden goToMain() instanserers loader fra klassen FXMLLoader.
+	 * Herefter benyttes setLocation til at fortælle, hvilket view vi gerne vil benytte.
+	 * variablen mainView sættes til at være lig med dette view, som er et GridPane.
+	 * Der instanseres nu en Scene, som skal være mainView, hvorefter vises dette view.
+	 * Hvis lokationen til viewet ikke kan findes smides der en IOException, hvor der bruges
+	 * printStackTrace, således man kan se hvad fejlen er og hvor i koden fejlen opstår.
+	 */
 	public void goToMain() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
@@ -53,6 +71,14 @@ public class MainController extends Application {
 		}
 	}
 
+	/**
+	 * Med metoden goToArrangeMeeting() instanserers loader fra klassen FXMLLoader.
+	 * Herefter benyttes setLocation til at fortælle, hvilket view vi gerne vil benytte.
+	 * variablen arrangeMeetingView sættes til at være lig med dette view, som er et AnchorPane.
+	 * Der instanseres nu en Scene, som skal være arrangeMeetingView, hvorefter dette view.
+	 * Hvis lokationen til viewet ikke kan findes smides der en IOException, hvor der bruges
+	 * printStackTrace, således man kan se hvad fejlen er og hvor i koden fejlen opstår
+	 */
 	public void goToArrangeMeeting(){
 		try {
             FXMLLoader loader = new FXMLLoader();
@@ -68,6 +94,15 @@ public class MainController extends Application {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * Med metoden goToPreviousSleep() instanserers loader fra klassen FXMLLoader.
+	 * Herefter benyttes setLocation til at fortælle, hvilket view vi gerne vil benytte.
+	 * variablen previousSleepView sættes til at være lig med dette view, som er et AnchorPane.
+	 * Der instanseres nu en Scene, som skal være previousSleepView, hvorefter dette view.
+	 * Hvis lokationen til viewet ikke kan findes smides der en IOException, hvor der bruges
+	 * printStackTrace, således man kan se hvad fejlen er og hvor i koden fejlen opstår
+	 */
 	public void goToPreviousSleep() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
@@ -83,11 +118,19 @@ public class MainController extends Application {
 		}
 	}
 
+	/**
+	 * Med metoden goToSleepHabits() instanserers loader fra klassen FXMLLoader.
+	 * Herefter benyttes setLocation til at fortælle, hvilket view vi gerne vil benytte.
+	 * variablen sleepHabitsView sættes til at være lig med dette view, som er et AnchorPane.
+	 * Der instanseres nu en Scene, som skal være sleepHabitsView, hvorefter dette view.
+	 * Hvis lokationen til viewet ikke kan findes smides der en IOException, hvor der bruges
+	 * printStackTrace, således man kan se hvad fejlen er og hvor i koden fejlen opstår
+	 */
 	public void goToSleepHabits() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainController.class.getResource("/SleepHabitsView.fxml"));
-			previousSleepView = (AnchorPane) loader.load();
+			sleepHabitsView = (AnchorPane) loader.load();
 			Scene scene = new Scene(sleepHabitsView);
 			primaryStage.setScene(scene);
 
@@ -98,6 +141,10 @@ public class MainController extends Application {
 		}
 	}
 
+	/**
+	 * Med metoden Logout() sættes user_id, User_firstname og user_pass til at være null i klassen UserModel.
+	 * Herefter klades metoden goToLogin fra klassen LoginController, hvor loginView sættes til at være primaryStage.
+	 */
     public void Logout(){
 		new UserModel().setUser_id(null);
 		new UserModel().setUser_firstname(null);
